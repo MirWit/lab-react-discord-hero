d542f7dc8e77cda583bcd374c148f6376fa6427f
Mira Witte <mira.witte@gmx.de>
2024-04-17 18:45:03 +0200

    Lab solved

--- lab-react-discord-hero/src/App.jsx
+++ lab-react-discord-hero/src/App.jsx
@@ -2,23 +2,34 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="image-container">
+    <div className="main">
       <div className="nav-bar">
         <img
           className="dicsord-logo"
           src="./src/assets/discord-logo-white.png"
           alt="white discord logo"
         />
-
         <img
           className="menu-icon"
           src="./src/assets/menu-icon.png"
           alt="menu icon"
         />
       </div>
-      <div className="continer-background-image">
+
+      <div className="content">
+        <h1>IMAGINE A PLACE...</h1>
+        <p>
+          ...ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+          diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
+        </p>
+        <button className="button-one">Download for Mac</button>
+        <button className="button-two">Open Dicsord in you browser</button>
+
         <img
-          className="background-image"
+          className="people-image"
           src="./src/assets/discord-background.png"
           alt="blue background"
         />
